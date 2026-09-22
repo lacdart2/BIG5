38e2e7dd51fecbff5acf635ed021b111bfe9e44d
Lakhdar Hafsi <40460234+lacdart2@users.noreply.github.com>
2026-09-22 17:15:24 +0200

    feat: add Champions League competition

--- src/types/league.ts
+++ src/types/league.ts
@@ -5,12 +5,16 @@ export interface League {
     shortName: string
     country: string
     colorHex: string
+    supportsStandings?: boolean
 }
 
 export const LEAGUES: League[] = [
-    { id: 'pl', apiId: 39, name: 'Premier League', shortName: 'PL', country: 'England', colorHex: '#38003C' },
-    { id: 'laliga', apiId: 140, name: 'La Liga', shortName: 'La Liga', country: 'Spain', colorHex: '#EE2523' },
-    { id: 'seriea', apiId: 135, name: 'Serie A', shortName: 'Serie A', country: 'Italy', colorHex: '#008FD7' },
-    { id: 'bundesliga', apiId: 78, name: 'Bundesliga', shortName: 'Bundesliga', country: 'Germany', colorHex: '#D3010C' },
-    { id: 'ligue1', apiId: 61, name: 'Ligue 1', shortName: 'Ligue 1', country: 'France', colorHex: '#091C3E' },
+    { id: 'pl', apiId: 39, name: 'Premier League', shortName: 'PL', country: 'England', colorHex: '#38003C', supportsStandings: true },
+    { id: 'laliga', apiId: 140, name: 'La Liga', shortName: 'La Liga', country: 'Spain', colorHex: '#EE2523', supportsStandings: true },
+    { id: 'seriea', apiId: 135, name: 'Serie A', shortName: 'Serie A', country: 'Italy', colorHex: '#008FD7', supportsStandings: true },
+    { id: 'bundesliga', apiId: 78, name: 'Bundesliga', shortName: 'Bundesliga', country: 'Germany', colorHex: '#D3010C', supportsStandings: true },
+    { id: 'ligue1', apiId: 61, name: 'Ligue 1', shortName: 'Ligue 1', country: 'France', colorHex: '#091C3E', supportsStandings: true },
+    { id: 'ucl', apiId: 2001, name: 'UEFA Champions League', shortName: 'UCL', country: 'Europe', colorHex: '#0B1F5E', supportsStandings: false },
 ]
+
+export const DOMESTIC_LEAGUES = LEAGUES.filter((league) => league.supportsStandings)
